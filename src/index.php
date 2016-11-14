c5f3041b99f147291c4ef3426b5bc9f0d43cc362
Kettu <fuchs.shcuf@gmail.com>
2016-11-14 02:06:58 +0100

    Displaying links grouped by internal and external and with working links to the original reference

--- src/index.php
+++ src/index.php
@@ -13,12 +13,12 @@
 	<main>
 		
 		<div class="divform">
-			<form id="foobar" action="./index.php" method="get">
+			<form id="foobar" action="index.php" method="get">
 				<label for="urlinput">URL</label>
 				<input type="text" id="urlinput" name="urlinput" size="50" required pattern="^\S*$"></input> <!-- pattern="^\S*$" does not allow space in input -->
 				<button>Search for links</button>
 			</form>
-			<a href="./about.html"><button>About</button></a>
+			<a href="about.html"><button>About</button></a>
 			
 		</div>
 		
@@ -26,10 +26,14 @@
 			<?php 
 				if( isset($_GET["urlinput"]) )
 				{
-					echo "<div>show the input: ";
-					echo $_GET["urlinput"];
+					//~ echo "<div>show the input: ";
+					echo "<h1 id=\"searchURL\">" . $_GET["urlinput"] . ":</h1>";
+					echo "<div id=\"resultNav\"> "
+							. "<p><a href=#internalResult>Internal Links</a></p>"
+							. "<p><a href=#externalResult>External Links</a></p>"
+							. "</div>";
 					echo main( $_GET["urlinput"] );
-					echo "</div>";
+					//~ echo "</div>";
 				}
 			?>
 		</div>
--- src/php/main.php
+++ src/php/main.php
@@ -3,6 +3,84 @@
 	include "DatabaseIO.php";
 
 
+	function getDomainName( $url )
+	{
+		$dnStart = 0;
+		if( strpos( $url, "https://" ) === 0 )
+			$dnStart = 8;
+		else if( strpos( $url, "http://" ) === 0 )
+			$dnStart = 7;
+			
+		if( strpos( $url, "www" ) !== false )
+			$dnStart += 4;
+
+		$dnStop = strpos( $url, "/", $dnStart );
+		
+		if( $dnStop === false )
+			$domainName = substr( $url, $dnStart );
+		else
+			$domainName = substr( $url, $dnStart, $dnStop - $dnStart );
+		
+		echo "<p>domain name is: $domainName</p>";
+		
+		return $domainName;
+	}
+	
+	function getUrlScheme( $url )
+	{
+		$dnStop = 0;
+		if( strpos( $url, "https://" ) === 0 )
+			$dnStop = 8;
+
+		else if( strpos( $url, "http://" ) === 0 )
+			$dnStop = 7;
+
+		// including the dot
+		if( strpos( $url, "www" ) !== false )
+			$dnStop += 4;
+		
+		$urlScheme = substr( $url, 0, $dnStop );
+		
+		echo "<p>url scheme name is: $urlScheme</p>";
+		
+		return $urlScheme;
+	}
+	
+	
+	function printOutput( $url, $linkList )
+	{
+		$internalString = "<div id=\"internalResult\"><h2>Internal Links</h2><ul>";
+		$externalString = "<div id=\"externalResult\"><h2>External Links</h2><ul>";
+
+		$domainName = getDomainName( $url );
+		$urlScheme = getUrlScheme( $url );
+
+		$pattern = "/www|http|https/";
+		
+		for( $id = 0; $id < count( $linkList ); $id++ )
+		{
+			$eval = preg_match( $pattern, $linkList[$id] );
+			$domainNameContained = strpos( $linkList[$id], $domainName );
+			
+			// consider everything that does start with www, http or https
+			// and does not contain the home address (after www, http or https) as external link
+			if( $eval === 1 && $domainNameContained === false )
+				$externalString .= "<li><a href=\"" . $linkList[$id] . "\">" . $linkList[$id] . "</a></li>";
+			else			
+				if( $domainNameContained !== false ) 	//TODO: to be even more precise if urlScheme is contained should be checked also
+					$internalString .= "<li><a href=\"" . $linkList[$id] . "\">" . $linkList[$id] . "</a></li>";
+				else
+					$internalString .= "<li><a href=\"" . $urlScheme . $domainName . "/" . $linkList[$id] . "\">" . $linkList[$id] . "</a></li>";	
+		}
+		
+		$internalString .= "</ul></div>";
+		$externalString .= "</ul></div>";
+		
+		echo $internalString;
+		echo $externalString;
+	}
+	
+
 	function main( $targetURL )
 	{	
 		// get the html source code 
@@ -38,6 +116,24 @@
 			$dbIO->writeLinkListToDB( $linksearch->getTargetURL(), $linksearch->getLinkList() );
 			
 			//TODO print links
+			// consider everything that does start with www, http, https, ftp or ftps 
+			// and does not contain the home address (after www, http,...) as external link
+			// use regular expressions
+			
+			// build two strings
+			
+			//~ $pattern = "/<a\s+.*?href=[\"\']?([^\"\']*\.[^\"\']*)[\"\'][^>]*>.*?<\/a>/i";
+			//~ $pattern = "/<a\s+.*?href=[\"\']?([^\"\']*\.[^\"\']*)[\"\'][^>]*>.*?<\/a>/i";
+				
+			//~ preg_match( $pattern, $subject, $match );
+				
+				// matches is a 2-dimensional array with the links in the second dimension.
+			//~ $this->linkList = $matches[1];
+			
+			printOutput( $linksearch->getTargetURL(), $linksearch->getLinkList() );
 		}
 	}
+	
+
+	
 ?>
